3bafd2b1feec63e2492006a2c48217a6669416dd
Prosper Alex <chigoziealex011@gmail.com>
2025-10-22 22:39:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#12a1a1",
+        "background-light": "#f6f8f8",
+        "background-dark": "#112121",
+      },
+      fontFamily: {
+        display: ["Spline Sans", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "0.5rem",
+        lg: "1rem",
+        xl: "1.5rem",
+        full: "9999px",
+      },
+    },
+  },
+};
